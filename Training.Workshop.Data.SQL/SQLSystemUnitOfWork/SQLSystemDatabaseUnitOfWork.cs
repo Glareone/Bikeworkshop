fa09ee7329fdeae7859cc6de87b380f4cc3efff0
Glareone <enlarius@gmail.com>
2014-08-07 18:36:32 +0300

    40% SQl Client

--- Training.Workshop.Data.SQL/SQLSystemUnitOfWork/SQLSystemDatabaseUnitOfWork.cs
+++ Training.Workshop.Data.SQL/SQLSystemUnitOfWork/SQLSystemDatabaseUnitOfWork.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using Training.Workshop.Domain.Entities;
 
 namespace Training.Workshop.Data.SQL.SQLSystemUnitOfWork
 {
     class SQLSystemDatabaseUnitOfWork:ISQLUnitOfWork
     {
+        /// <summary>
+        /// Connection string to SQL database
+        /// </summary>
+        private static SqlConnection connectionstring = new SqlConnection(
+            "Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI");
+        
+        /// <summary>
+        /// SQL DataReader Class
+        /// </summary>
+        private SqlDataReader sqldatareader = null;
+        
         public SQLSystemDatabaseUnitOfWork(SQLSystemDatabaseUnitofWorkFactory unitofworkfactory)
-        { 
-            //TODO
-            //need realisation
+        {
+            try 
+            {
+             SqlCommand cmd = new SqlCommand("select * from Users", connectionstring);
+             sqldatareader= cmd.ExecuteReader();
+                 while (sqldatareader.Read())
+                {
+                 //TODO
+                 //need realisation
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException();
+            }
         }
 
 
