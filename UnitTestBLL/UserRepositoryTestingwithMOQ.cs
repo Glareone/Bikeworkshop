e24a3f25e8a8df76cf3c7d9dbdd421a644d9a1f3
Glareone <enlarius@gmail.com>
2014-08-28 17:17:36 +0300

    1
    1

---
+++ UnitTestBLL/UserRepositoryTestingwithMOQ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestBLL
+{
+    [TestClass]
+    public class UserRepositoryTestingwithMOQ
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
