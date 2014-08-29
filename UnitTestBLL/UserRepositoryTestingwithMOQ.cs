bb851bfce19e64cc88ffc801a242b6f4e4a55049
Glareone <enlarius@gmail.com>
2014-08-29 13:09:04 +0300

    1

--- UnitTestBLL/UserRepositoryTestingwithMOQ.cs
+++ UnitTestBLL/UserRepositoryTestingwithMOQ.cs
@@ -9,6 +9,7 @@ using Training.Workshop.Domain.Services;
 using Training.Workshop.Service;
 using Training.Workshop.UnitOfWork;
 using Training.Workshop.Domain.Entities;
+using Training.Workshop.Data;
 
 namespace UnitTestBLL
 {
@@ -24,21 +25,37 @@ namespace UnitTestBLL
 
             Training.Workshop.UnitOfWork.Context.Current.UnitOfWorkFactory = new Training.Workshop.Data.SQL.SQLSystemUnitOfWork.SQLSystemDatabaseUnitofWorkFactory();
 
+
+            //Constructing mock on service.
             var iuserservice = new Mock<IUserService>();
             
-            var user=new User
-            {
-                Username="glareone",
-                Roles=new List<Role>()
-            };
-            iuserservice.Setup(m => m.GetUser("glareone", "glareone")).Returns(user);
+            string username = "testinguser";
+            
+            var listofpermission = new List<string>{"ReadAll","WriteAll","DeleteAll"};
+            
+            var role1 = new Role{Name="administrator",Permissions=listofpermission};
+            
+            var roles = new List<Role> { role1 };
+            
+            iuserservice.Setup(m => m.GetUser("glareone", "glareone")).Returns(new User { Username=username, Roles=roles});
 
             string[] role=new string[]{"customer"};
 
-            iuserservice.Setup(m=> m.Create("newuser","newuser",role)).Returns(
+            iuserservice.Setup(m => m.Create("newuser", "newuser", role)).Returns(new User { Username=username,Roles=roles});
 
 
+            //Create mock on userrepository
+            string repositorytestusername="testuser";
 
+            string repositorytestpassword="testpassword";
+
+            var userrepository = new Mock<IUserRepository>();
+            //configuring userrepository mock
+            userrepository.Setup(m => m.GetUser(repositorytestusername, repositorytestpassword)).Returns(new User { Username = username, Roles = roles });
+            
+            userrepository.Setup(m=>m.RetrieveAllUsers()).Returns(new List<User>{new User{Username=username,Roles=roles}});
+            //test userservice based on mocked userrepository
+            var service
         }
     }
 }
